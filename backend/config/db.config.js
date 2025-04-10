a35c2cab9d4427a73f8238e1db49d47b32156773
Denning01 <gichabadenning254@gmail.com>
2025-04-10 16:25:17 +0300

    fixed a database error

--- backend/config/db.config.js
+++ backend/config/db.config.js
@@ -3,9 +3,9 @@ require('dotenv').config();
 
 const pool = new Pool({
   user: process.env.DB_USER || 'postgres',
-  host: process.env.DB_HOST || 'localhost',
+  host: process.env.DB_HOST || 'db',
   database: process.env.DB_NAME || 'todo_db',
-  password: process.env.DB_PASSWORD || 'postgres',
+  password: process.env.DB_PASSWORD || '1234',
   port: process.env.DB_PORT || 5432,
 });
 
